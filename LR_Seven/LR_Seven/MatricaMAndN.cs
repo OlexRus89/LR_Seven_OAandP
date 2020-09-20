6bb9d888da990e3ab26fe07b2b633bdfdcb4ae9a
Alexey Svidnitskiy <lesha.svid@yandex.ru>
2020-09-21 00:38:32 +0300

    Лабораторная работа 7. Основы алгоритмизации и программирования

---
+++ LR_Seven/LR_Seven/MatricaMAndN.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+using System.ComponentModel;
+
+namespace LR_Seven
+{
+    class MatricaMAndN
+    {
+        /*Класс «Матрица MxN». Реализовать инициализацию элементов
+         *матрицы случайными числами, вывод матрицы, нахождение
+         *максимального и минимального элементов, а также вывод
+         *информации об объекте.  
+         */
+
+        public int M, N;
+
+        public MatricaMAndN() { }
+
+        public MatricaMAndN(int Mm, int Nm)
+        {
+            M = Mm;
+            N = Nm;
+        }
+
+        public MatricaMAndN CreateMatricaFromFile()
+        {
+            this.M = Convert.ToInt32(Console.ReadLine());
+            this.N = Convert.ToInt32(Console.ReadLine());
+            return new MatricaMAndN(M,N);
+        }
+
+        public (int, int) Load()
+        {
+            this.M = Convert.ToInt32(Console.ReadLine());
+            this.N = Convert.ToInt32(Console.ReadLine());
+            return (M, N);
+        }
+
+        public double[,] random(int M1, int N1)
+        {
+            this.M = M1;
+            this.N = N1;
+            Random r = new Random(DateTime.Now.Millisecond);
+            double[,] mas = new double[M, N];
+            int x = 0;
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    mas[i,j] = r.Next(-1000, 1001);
+                }
+            }
+            return mas;
+        }
+
+        public string[,] InfoMatrica(int M2, int N2)
+        {
+            this.M = M2;
+            this.N = N2;
+            String[,] mas1 = new String[M, N];
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    mas1[i, j] = "* ";
+                }
+            }
+            return mas1;
+        }
+
+        public void Info()
+        {
+#if DEBUG
+            Console.WriteLine("введите данные M и N:");
+#endif
+            var typle = Load();
+#if DEBUG
+            Console.WriteLine();
+#endif
+            Console.WriteLine("Была введена матрица размером {0}x{1}", typle.Item1, typle.Item2);
+            Console.WriteLine("Результаты следующие: ");
+            InfoMatrica(typle.Item1, typle.Item2);
+            Console.WriteLine();
+            random(typle.Item1, typle.Item2);
+        }
+        
+#if DEBUG
+        public void Info (ConsoleColor fg, ConsoleColor bgc)
+        {
+            Console.ForegroundColor = fg;
+            Console.BackgroundColor = bgc;
+            Console.Clear();
+            Info();
+
+        }
+#endif
+    }
+}
---
+++ LR_Seven/LR_Seven/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Dynamic;
+using System.IO;
+
+namespace LR_Seven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Вариант 27
+
+#if !DEBUG
+            TextWriter save_out = Console.Out;
+            TextReader save_in = Console.In;
+            var new_out = new StreamWriter(@"matrica_output.txt");
+            var new_in = new StreamReader(@"matrica_input.txt");
+            Console.SetOut(new_out);
+            Console.SetIn(new_in);
+#endif
+ 
+            MatricaMAndN load = new MatricaMAndN();
+#if !DEBUG
+            load.Info();
+#endif
+
+#if DEBUG
+            load.Info(ConsoleColor.Black, ConsoleColor.Red);
+#endif
+
+            double[,] array = load.random(load.M, load.N);
+            String[,] array1 = load.InfoMatrica(load.M, load.N);
+            double Max = -1001, Min = 1001;
+
+            for (int i = 0; i < load.M; i++)
+            {
+                for (int j = 0; j < load.N; j++)
+                {
+                    Console.Write(array1[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < load.M; i++)
+            {
+                for (int j = 0; j < load.N; j++)
+                {
+                    Console.Write(array[i,j] + " ");
+                    if (Max <= array[i, j])
+                    {
+                        Max = array[i, j];
+                    }
+                    if (Min >= array[i,j])
+                    {
+                        Min = array[i, j];
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Максимальный элемент: {0}", Max);
+            Console.WriteLine("Минимальный элемент: {0}", Min);
+
+#if !DEBUG
+            Console.SetOut(save_out); new_out.Close();
+            Console.SetIn(save_in); new_in.Close();
+#endif
+
+#if DEBUG
+            Console.ReadKey();
+#endif
+
+        }
+    }
+}
